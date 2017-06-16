30bc9a2810677e26d8c6a82f4f33e147219a5ba9
Sérgio Gonçalves <eu.sergiogoncalves@gmail.com>
2017-06-15 22:07:12 -0400

    Local files

---
+++ spec/exempleSpec.js
@@ -0,0 +1,9 @@
+describe("Suite de teste de exemplo",function(){
+    it("Deve demonstrar o uso do Jasmine com NodeJS", function(){
+        expect(true).toEqual(true);
+    });
+    
+    it("Demonstrar o uso do karma em modo de escuta", function(){
+        expect(1).toEqual(1);
+    });
+});
